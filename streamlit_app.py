abdae4cf1de14c1829aac89e81e3463dce3f12c4
katokid1818 <85013388+katokid1818@users.noreply.github.com>
2022-06-21 18:29:03 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,32 +2,12 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 
-import seaborn as sns
 import matplotlib.pyplot as plt
 import os
 import keras
 import tensorflow as tf
 
-from keras.models import Sequential
-from keras.layers import Dense
-from keras.callbacks import ModelCheckpoint
-from keras import datasets,Sequential,callbacks
-from keras.layers import Conv2D, Dense, MaxPooling2D, Flatten
-from sklearn.preprocessing import StandardScaler
-from sklearn.model_selection import train_test_split
-from keras.layers import Dense,Activation,BatchNormalization,Dropout
-from keras.models import Sequential
-from tensorflow.keras.utils import to_categorical
-from keras import callbacks
-from sklearn.metrics import precision_score, recall_score,confusion_matrix,classification_report,accuracy_score,f1_score
-from keras.callbacks import EarlyStopping
-from tensorflow.keras.optimizers import RMSprop
-from matplotlib import scale
-from sklearn.metrics import confusion_matrix
-from keras.utils import np_utils
 from keras.preprocessing.image import load_img, img_to_array,array_to_img,ImageDataGenerator, image
-from tensorflow.keras.optimizers import RMSprop, SGD
-import os
 
 from keras.models import load_model
 
