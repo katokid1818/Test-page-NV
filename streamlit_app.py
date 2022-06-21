8597d3abfe6874b25201de3d0e4458cf34f90e7b
katokid1818 <85013388+katokid1818@users.noreply.github.com>
2022-06-21 17:56:22 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -10,3 +10,7 @@ DATA_URL = ('https://s3-us-west-2.amazonaws.com/'
 
 @st.cache
 uploaded_file = st.file_uploader("Choose a file")
+if uploaded_file is not None:
+     # To read file as bytes:
+     bytes_data = uploaded_file.getvalue()
+     st.write(bytes_data)
