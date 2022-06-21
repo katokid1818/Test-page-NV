ca3a35181ccba667cf53bee9627c449e4e5b0f52
katokid1818 <85013388+katokid1818@users.noreply.github.com>
2022-06-21 18:29:41 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,14 +2,6 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 
-import os
-import keras
-import tensorflow as tf
-
-from keras.preprocessing.image import load_img, img_to_array,array_to_img,ImageDataGenerator, image
-
-from keras.models import load_model
-
 st.title('Count the number of cows on the farm')
 
 DATA_URL = ('https://drive.google.com/file/d/14nQpk8gInuhtlLiWr6-WedQjmDmdRF1_/view?usp=sharing')
