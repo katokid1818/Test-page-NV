6f100c8e92b583842734847571b64cc3f8b7bea1
katokid1818 <85013388+katokid1818@users.noreply.github.com>
2022-06-21 17:45:33 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,7 +2,7 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 
-st.title('Hello World')
+st.title('Count the number of cows on the farm')
 
 DATE_COLUMN = 'date/time'
 DATA_URL = ('https://s3-us-west-2.amazonaws.com/'
